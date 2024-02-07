4d5f02504cde28be80467e145c49435d71b2a5e1
napvasconcelos <napvasconcelos@gmail.com>
2024-02-06 21:55:35 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nome_do_pacote',
+    name='icvb',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'numpy',
         'pandas',
         'os',
-        'matplotlib',
-        # Liste todas as dependências necessárias aqui
+        'matplotlib'
     ],
     # Outras informações opcionais, como descrição, autor, etc.
     description='ICVB',
     author='Nivaldo Vasconcelos',
     author_email='napvasconcelos@gmail.com',
-    url='https://github.com/neurobucano/allen-icvb/',
+    url='https://github.com/neurobucano/icvb/',
 )
